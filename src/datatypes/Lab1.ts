194c92dbdf41cd2c4727db4296c4f8c1ece5081f
Vietanh099 <108389138+Vietanh099@users.noreply.github.com>
2023-03-15 21:13:02 +0700

    Update lab1

--- src/datatypes/Lab1.ts
+++ src/datatypes/Lab1.ts
@@ -1,4 +1,20 @@
-//bubble sort
+const numArray3 = [13, 5, 8, 2, 3, 1];
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    let j = i -1;
+    while (j >= 0 && arr[j] > currentVal) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = currentVal;
+  }
+}
+
+insertionSort(numArray3);
+console.log(numArray3);
+
  const Array1:number[]=[2,3,9,1,8,11,4]
 function bubbleSort(arr:number[]):number[]{
     for(let i=0;i<arr.length ;i++){
@@ -14,7 +30,7 @@ function bubbleSort(arr:number[]):number[]{
 }
 console.log(bubbleSort(Array1));
 
-const array2:string[]=['Than_tran','Anh_tran',"cau_tran"];
+const array2:string[]=['Viet_Anh','Anh_tran',"cau_tran"];
 function bubbleSort2(arr:string[]):string[]{
     for(let i=0;i<arr.length;i++){
         for(let j=0;j<arr.length-i-1;j++){
